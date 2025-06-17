f78e382ac500cfa263929b6edcfcded09bff22d2
Kote87 <128540817+Kote87@users.noreply.github.com>
2025-06-17 16:32:33 +0200

    Remove setup.cfg and update setup.py

--- ws/src/scan_filter/setup.py
+++ ws/src/scan_filter/setup.py
@@ -8,17 +8,26 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
-        (os.path.join('share/ament_index/resource_index/packages'),
-         ['resource/scan_filter']),
-        (os.path.join('share/ament_index/resource_index/ros2_executable'),
-         ['resource/scan_filter__front_cone']),
-        (os.path.join('share', package_name), ['package.xml']),
+        (
+            os.path.join("share/ament_index/resource_index/packages"),
+            ["resource/" + package_name],
+        ),
+        (
+            os.path.join("share/ament_index/resource_index/ros2_executable"),
+            ["resource/scan_filter__front_cone"],
+        ),
+        (os.path.join("share", package_name), ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools", "rclpy"],
+    python_requires=">=3.8",
     zip_safe=True,
+    maintainer="Maintainer",
+    maintainer_email="maintainer@example.com",
+    description="Lidar front-cone filter for ROSbot XL",
+    license="Apache-2.0",
     entry_points={
-        'console_scripts': [
-            'front_cone = scan_filter.front_cone:main',
+        "console_scripts": [
+            "front_cone = scan_filter.front_cone:main",
         ],
     },
 )
